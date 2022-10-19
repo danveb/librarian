6d4fe0a86c176f9ca5c90e36f74759579ff1b379
Daniel Bae <danibae91@icloud.com>
2022-10-19 12:31:31 -0700

    work on Home page and styles/assets

---
+++ client/src/constants/links.js
@@ -0,0 +1,17 @@
+export const links = [
+    {
+        id: 1, 
+        url: "/",
+        text: "Home"
+    }, 
+    {
+        id: 2, 
+        url: "/library",
+        text: "Library"
+    }, 
+    {
+        id: 3, 
+        url: "/contact",
+        text: "Contact"
+    }, 
+];
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import "../styles/Home.css"; 
+import librarian from "../assets/librarian.jpg"; 
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to Librarian</h1>
-            <button className="btn"><Link to="/books">Go to Library</Link></button>
+        <div className="home">      
+            <img className="home__librarian" src={librarian} alt="librarian main" />      
+            <h1 className="home__title">Welcome to Librarian</h1>
+            <p className="home__subtitle">Powerful, extensible toolkit for those who share the passion of books.</p>
+            <button className="btn btn-main">
+                <Link to="/library">Go to Library</Link>
+            </button>
         </div>
     )
 }
